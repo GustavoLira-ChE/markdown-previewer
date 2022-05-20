4b14dc1bec275e53d8a956bff3ed67d9a4e545c9
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-20 16:14:32 -0500

    add: initial files and first component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import InputTextArea from "./components/InputTextArea";
+
+const App = () => {
+    return(
+        <div>
+            <InputTextArea />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/InputTextArea.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InputTextArea = () => {
+    return(
+        <div>
+            <h1>
+                Input Text Area
+            </h1>
+        </div>
+    );
+};
+
+export default InputTextArea;
