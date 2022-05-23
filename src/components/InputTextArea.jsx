96669a9fe58b50c8503ceabab905a67810a339b4
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-23 12:13:50 -0500

    fix: show unnecessary content

--- src/components/InputTextArea.jsx
+++ src/components/InputTextArea.jsx
@@ -6,18 +6,12 @@ const InputTextArea = () => {
 
     const { inputText ,inputTextHandler } = useContext(InputTextContext);
 
-    const updateMarkdown = () => {
-        const __html = DOMPurify.sanitize(inputText);
-        return { __html };
-    };
-
     return(
         <div id="editor">
             <h1>
                 Input Text Area
             </h1>
             <textarea rows="4" cols="50" style={{resize: "none"}} onChange={inputTextHandler} value={inputText}></textarea>
-            <div dangerouslySetInnerHTML={updateMarkdown()}></div>
         </div>
     );
 };
