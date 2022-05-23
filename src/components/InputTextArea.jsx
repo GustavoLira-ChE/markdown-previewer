5edee0fbc492e5d31bc47dbcd62fdd5742f24d0b
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-23 11:52:45 -0500

    feat: add custom hook to handle input text

--- src/components/InputTextArea.jsx
+++ src/components/InputTextArea.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useContext} from "react";
+import InputTextContext from "../context/InputTextContext";
+import DOMPurify from "dompurify";
 
 const InputTextArea = () => {
+
+    const { inputText ,inputTextHandler } = useContext(InputTextContext);
+
+    const updateMarkdown = () => {
+        const __html = DOMPurify.sanitize(inputText);
+        return { __html };
+    };
+
     return(
-        <div>
+        <div id="editor">
             <h1>
                 Input Text Area
             </h1>
+            <textarea rows="4" cols="50" style={{resize: "none"}} onChange={inputTextHandler} value={inputText}></textarea>
+            <div dangerouslySetInnerHTML={updateMarkdown()}></div>
         </div>
     );
 };
