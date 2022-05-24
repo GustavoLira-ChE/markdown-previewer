f275ca65d5438fe6884d55ac02dca5a64d7d00dc
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-24 12:31:29 -0500

    add: close button component and style

---
+++ src/components/CloseButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/closebutton.css';
+
+const CloseButton = () => {
+    return (
+        <button className="closeButton">X</button>
+    );
+};
+
+export default CloseButton;
