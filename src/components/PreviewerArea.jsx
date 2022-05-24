5639a73cb0cc26a8e45a8f63be12fd396f3ccc09
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-24 14:34:29 -0500

    add: prop that receive a handle toggle function of previewer

--- src/components/PreviewerArea.jsx
+++ src/components/PreviewerArea.jsx
@@ -1,15 +1,18 @@
 import React, { useContext } from "react";
 import InputTextContext from "../context/InputTextContext";
+import ToggleContext from "../context/ToggleContext";
 import { marked } from "marked";
 import DOMPurify from 'dompurify';
 import '../styles/previewerArea.css';
 import hljs from "highlight.js";
 import { Renderer } from "marked";
+import CloseButton from "./CloseButton";
 
 
 const PreviewerArea = () => {
 
     const { inputText } = useContext(InputTextContext);
+    const { handlePreviewerToggle } = useContext(ToggleContext);
 
     const updateMarkdown = () => {
         const textTransform = DOMPurify.sanitize(inputText);
@@ -30,9 +33,10 @@ const PreviewerArea = () => {
     });
     return(
         <div id="preview">
-            <h2 className="previewerTitle">
-                Preview Area
-            </h2>
+            <div className="previewHeader">
+                <h2 className="previewerTitle">Preview Area</h2>
+                <CloseButton func={handlePreviewerToggle} />
+            </div>
             <div className="previewerDisplay" dangerouslySetInnerHTML={updateMarkdown()}></div>
         </div>
     );
