c7fcde56e87989383f2ee12737e5e4e5643ef242
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-24 14:59:52 -0500

    add: context to handle show/hidden elements

--- src/containers/BodyApp.jsx
+++ src/containers/BodyApp.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import InputTextArea from '../components/InputTextArea';
 import PreviewerArea from '../components/PreviewerArea';
 import PreSyntaxUseList from './PreSyntaxUseList';
+import ToggleContext from "../context/ToggleContext";
 
 
 const BodyApp = () => {
+    const { editorToggle, previewerToggle, builtInToggle } = useContext(ToggleContext);
     return (
         <div className="body-container">
-            <InputTextArea />
-            <PreviewerArea />
-            <PreSyntaxUseList />
+            {editorToggle && <InputTextArea />}
+            {previewerToggle && <PreviewerArea />}
+            {builtInToggle && <PreSyntaxUseList />}
         </div>
     );
 };
