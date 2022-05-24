98c74fbbced3f0202bdb87280d47ef65d66aa939
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-24 11:39:49 -0500

    chore: added marked setoptions and highlight.js

--- src/components/PreviewerArea.jsx
+++ src/components/PreviewerArea.jsx
@@ -3,6 +3,9 @@ import InputTextContext from "../context/InputTextContext";
 import { marked } from "marked";
 import DOMPurify from 'dompurify';
 import '../styles/previewerArea.css';
+import hljs from "highlight.js";
+import { Renderer } from "marked";
+
 
 const PreviewerArea = () => {
 
@@ -13,7 +16,18 @@ const PreviewerArea = () => {
         const __html = marked(textTransform);
         return { __html };
     };
-    
+    marked.setOptions({
+        render: new Renderer(),
+        highlight: function (code) {
+            return hljs.highlightAuto(code).value;
+        },
+        gfm: true,
+        tables: true,
+        breaks: true,
+        smartLists: true,
+        smartypants: true,
+        xhtml: true
+    });
     return(
         <div id="preview">
             <h2 className="previewerTitle">
