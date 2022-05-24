e3e285c2a1b3b89b87d634bae0222ad4bc4a41c6
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-24 14:59:14 -0500

    feat: added Buttoon to show part hidden and style

---
+++ src/components/ShowButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/showButton.css';
+
+const ShowButton = ({buttonName ,state, func}) => {
+    
+    
+    return (
+        <li className='li-button'>
+            {state ? <button className='showButton' disabled>{buttonName}</button> : <button className='showButton' onClick={func}>{buttonName}</button>}
+        </li>
+    );
+};
+
+export default ShowButton;
