716eb6c42c5bbba903f3e5fdba5a9bb137926886
Burstniart <v4gr33n@hotmail.com>
2022-06-02 00:28:00 -0500

    Added Eslint to the project

--- src/containers/BodyApp.jsx
+++ src/containers/BodyApp.jsx
@@ -1,19 +1,19 @@
-import React, {useContext} from 'react';
-import InputTextArea from '../components/InputTextArea';
-import PreviewerArea from '../components/PreviewerArea';
-import PreSyntaxUseList from './PreSyntaxUseList';
+import React, {useContext} from "react";
+import InputTextArea from "../components/InputTextArea";
+import PreviewerArea from "../components/PreviewerArea";
+import PreSyntaxUseList from "./PreSyntaxUseList";
 import ToggleContext from "../context/ToggleContext";
 
 
 const BodyApp = () => {
-    const { editorToggle, previewerToggle, builtInToggle } = useContext(ToggleContext);
-    return (
-        <div className="body-container">
-            {builtInToggle && <PreSyntaxUseList />}
-            {editorToggle && <InputTextArea />}
-            {previewerToggle && <PreviewerArea />}
-        </div>
-    );
+  const { editorToggle, previewerToggle, builtInToggle } = useContext(ToggleContext);
+  return (
+    <div className="body-container">
+      {builtInToggle && <PreSyntaxUseList />}
+      {editorToggle && <InputTextArea />}
+      {previewerToggle && <PreviewerArea />}
+    </div>
+  );
 };
 
 export default BodyApp;
--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,21 +1,21 @@
-import React, {useContext} from 'react';
-import ShowButton from '../components/ShowButton';
-import ToggleContext from '../context/ToggleContext';
-import '../styles/header.css';
+import React, {useContext} from "react";
+import ShowButton from "../components/ShowButton";
+import ToggleContext from "../context/ToggleContext";
+import "../styles/header.css";
 
 
 const Header = () => {
-    const {previewerToggle, handlePreviewerToggle, editorToggle, handleEditorToggle, builtInToggle, handleBuiltInToggle} = useContext(ToggleContext);
-    return (
-        <div className='header-container'>
-            <h1 className='main-title'>Markdown Previewer</h1>
-            <nav className='navbar-button'>
-                <ShowButton buttonName={'Editor'} state={editorToggle} func={handleEditorToggle} />
-                <ShowButton buttonName={'Previewer'} state={previewerToggle} func={handlePreviewerToggle} />
-                <ShowButton buttonName={'BuiltIn'} state={builtInToggle} func={handleBuiltInToggle} />
-            </nav>
-        </div>
-    );
+  const {previewerToggle, handlePreviewerToggle, editorToggle, handleEditorToggle, builtInToggle, handleBuiltInToggle} = useContext(ToggleContext);
+  return (
+    <div className='header-container'>
+      <h1 className='main-title'>Markdown Previewer</h1>
+      <nav className='navbar-button'>
+        <ShowButton buttonName={"Editor"} state={editorToggle} func={handleEditorToggle} />
+        <ShowButton buttonName={"Previewer"} state={previewerToggle} func={handlePreviewerToggle} />
+        <ShowButton buttonName={"BuiltIn"} state={builtInToggle} func={handleBuiltInToggle} />
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
